2c83552161c0030b6984b4233ca6d89997713696
KateVasilevskaya <7091764@gmail.com>
2022-08-15 21:27:08 +0200

    guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.value = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.max = max;
+    this.min = min;
+  }
 
+  guess() {
+    this.value = Math.floor(this.min + (this.max - this.min + 1) / 2);
+    return this.value;
+  }
+
+  lower() {
+    if (this.value < this.max) {
+      this.max = this.value;
     }
+  }
 
-    guess() {
-
+  greater() {
+    if (this.value > this.min) {
+      this.min = this.value;
     }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  }
 }
 
 module.exports = GuessingGame;
